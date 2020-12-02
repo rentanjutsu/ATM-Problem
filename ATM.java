5a50081f84bd3e7a64105964020fe7170853db70
rentanjutsu <74683663+rentanjutsu@users.noreply.github.com>
2020-12-02 16:14:21 -0500

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -7,21 +7,21 @@ import java.util.Scanner;
       int amount = 0;
       //Prompt the user for amount in dollars
       System.out.print("Enter the amount: ");
-      amount = userInput.nextLine.int();
+      amount = userInput.nextInt();
       if (amount > 300) {//Print limit exceeded
       System.out.println("Limit of $300 exceeded!");
       }
       if (amount <= 300 && (amount > 0)); {//Calculate bills by denomination
-      d20 = (amount / 20);
-      d10 = ((amount % 20) / 10);
-      d5 = (((amount % 20) % 10) / 5);
-      d1 = ((((amount %20) % 10) % 5) / 1);
+      int d20 = (amount / 20);
+      int d10 = ((amount % 20) / 10);
+      int d5 = (((amount % 20) % 10) / 5);
+      int d1 = ((((amount %20) % 10) % 5) / 1);
       System.out.println("Bills by denominations: " + 
-      "/nt$20: " + d20 + 
-      "/nt$10: " + d10 + 
-      "/nt$5: " + d5 + 
-      "/nt$1: " + d1 +
-      "/n(amount) = " + "((d20) * $20)" + "((d10) * $10" + "((d5) * $5)" + "((d1) + * $1");
+      "\n\t$20: " + 20 + 
+      "\n\t$10: " + 10 + 
+      "\n\t$5: " + 5 + 
+      "\n\t$1: " + 1 +
+      "\n"amount + " = " + "(" + d20 + " * $20)" + "(" + d10 + " * 10)" + "(" + d5 + " * 5)" + "(" + d1 + " * 1)"
       }
    }
 }
